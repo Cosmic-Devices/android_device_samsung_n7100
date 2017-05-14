f258726a425b763c0d4728231f9ff56cc931989b
Sagar Shah <sagarshah2326@gmail.com>
2017-05-14 23:17:05 +0530

    Import RIL related includes
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- shims/libsecril-shim/Android.mk
+++ shims/libsecril-shim/Android.mk
@@ -5,5 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= secril-shim.c
 LOCAL_SHARED_LIBRARIES := liblog libbinder
 LOCAL_MODULE:= libsecril-shim
+LOCAL_C_INCLUDES := hardware/ril/include
 
 include $(BUILD_SHARED_LIBRARY)
